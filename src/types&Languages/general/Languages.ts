8c1981147d33ea8c2b64887b2d2c7cb2fd698b9f
haimfrenkel <hf36274@gmail.com>
2022-01-05 18:25:00 +0200

    init commit

---
+++ src/types&Languages/general/Languages.ts
@@ -0,0 +1,31 @@
+
+export const languages = {
+    SHTIBEL: {
+        HE: 'שטיבל',
+        EN: 'shtibel'
+    },
+    MEN: {
+        HE: 'גבר',
+        EN: 'men'
+    },
+    WOMEN: {
+        HE: 'אישה',
+        EN: 'women'
+    },
+    MARRIED: {
+        HE: 'נשוי\נשואה',
+        EN: 'married'
+    },
+    SINGLE: {
+        HE: 'רווק\רווקה',
+        EN: 'single'
+    },
+    WIDOW: {
+        HE: 'אלמן\אלמנה',
+        EN: 'window/er'
+    },
+    DIVORCED: {
+        HE: 'גרוש\גרושה',
+        EN: 'divorced'
+    }
+}
